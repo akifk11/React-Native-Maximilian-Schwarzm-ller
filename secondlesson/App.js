729b5bc432999ce64a83004779b5be9221b04e49
akifk11 <69064758+akifk11@users.noreply.github.com>
2022-07-22 23:34:21 +0300

    flexbox

--- secondlesson/App.js
+++ secondlesson/App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.blockBlue}>
+        <Text style={styles.textColor}>1</Text>
+      </View>
+      <View style={styles.blockRed}>
+        <Text style={styles.textColor}>2</Text>
+      </View>
+      <View style={styles.blockGreen}>
+        <Text style={styles.textColor}>3</Text>
+      </View>
+   </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop:50,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flexDirection:"row",
+    },
+    blockBlue:{
+      backgroundColor:"blue", 
+      width:100,
+      height:100,  
+      justifyContent:"center",
+      alignItems:"center",
+    },
+    blockRed:{
+      backgroundColor:"red",   
+      width:100,
+      height:100,  
+      justifyContent:"center",
+      alignItems:"center",
+    },
+    blockGreen:{
+      backgroundColor:"green",   
+      width:100,
+      height:100,  
+      justifyContent:"center",
+      alignItems:"center",
+    },
+    textColor:{
+      color:"white",
+    }
+
+
 });
